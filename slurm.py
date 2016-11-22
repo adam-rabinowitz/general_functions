2e262d69a30e1428bd4a1cf178bc2a1ab570be09
adam-rabinowitz <Adam.Rabinowitz@crick.ac.uk>
2016-11-22 13:00:33 +0000

    Modified memory to be in gigabytes

--- slurm.py
+++ slurm.py
@@ -10,20 +10,26 @@ class submitJobs(object):
         self.commandDict = collections.OrderedDict()
     
     def add(
-            self, command, processors = 1, memory = 4000, stdout = '/dev/null',
-            stderr = '/dev/null', depend = []
+            self, command, processors = 1, memory = 4, stdout = '/dev/null',
+            stderr = '/dev/null', modules = [], depend = []
         ):
         ''' command to add commands to job dictionary.
         
         Args:
             command (str)- Command to submit.
             processors (int)- Number of processors.
-            memory (int)- Memory per processor in megabytes.
+            memory (int)- Memory per processor in gigabytes.
             stdout (str)- Full path to stdout.
             stderr (str)- Full path to stderr.
             depend (list)- A list of job ids returned from object.
 
         '''
+        # check modules
+        if not isinstance(modules, list):
+            raise IOError('modules must be a list')
+        for m in modules:
+            if not isinstance(m, str):
+                raise IOError('modules must be a list of strings')
         # check dependencies
         if not isinstance(depend, list):
             raise IOError('dependencies must be a list')
@@ -33,8 +39,8 @@ class submitJobs(object):
         # check processor arguments
         if not isinstance(processors, int):
             raise IOError('processors argument must be an integer')
-        if processors < 0 or processors > 12:
-            raise IOError('processors argument must be >= 1 and <= 12')
+        if processors < 0 or processors > 16:
+            raise IOError('processors argument must be >= 1 and <= 16')
         # modify input command
         if isinstance(command, list):
             command = ' '.join(command)
@@ -49,14 +55,8 @@ class submitJobs(object):
             raise IOError('Stderr directory does not exist')
         # add command to dictionary and return command number
         commandNo = len(self.commandDict)
-        self.commandDict[commandNo] = (
-            command,
-            processors,
-            memory,
-            stdout,
-            stderr,
-            depend
-        )
+        self.commandDict[commandNo] = (command, processors, memory, stdout,
+            stderr, modules, depend)
         return(commandNo)
     
     def submit(self, verbose = False):
@@ -69,13 +69,21 @@ class submitJobs(object):
         # extract commands and parameters
         for commandNo in self.commandDict:
             # Unpack command and parameters
-            command, processors, memory, stdout, stderr, depend = (
+            command, processors, memory, stdout, stderr, modules, depend = (
                 self.commandDict[commandNo])
+            # Set required modules
+            moduleCommand = 'module purge'
+            if modules:
+                moduleOut = ' '.join(modules)
+                moduleCommand += ' && module load {}'.format(' '.join(modules))
+            else:
+                moduleOut = None
+            completeCommand = '{} && {}'.format(moduleCommand, command)
             # create slurm command and add node informaion
-            slurmCommand = ['sbatch', '--wrap', '"'+ command + '"',
+            slurmCommand = ['sbatch', '--wrap', '"'+ completeCommand + '"',
                 '-c', str(processors), '-o', stdout, '-e', stderr,
-                '--mem-per-cpu', str(memory)]
-            # Add dependecy
+                '--mem-per-cpu', str(memory) + 'G']
+            # Add dependecies to slurm command
             dependList = []
             for d in depend:
                 dependList.append(slurmJobList[d][0])
@@ -85,10 +93,10 @@ class submitJobs(object):
                 dependOut = ' '.join(dependList)
             else:
                 dependOut = None
-            # Try submit the job ten times
-            slurmID = None
+            # Try and submit complete command ten times
             slurmCommand = ' '.join(slurmCommand)
-            for _ in range(10):
+            slurmID = None
+            for count in range(10):
                 slurmOut = subprocess.check_output(slurmCommand, shell=True)
                 slurmMatch = slurmRegx.match(slurmOut)
                 if slurmMatch is None:
@@ -100,7 +108,7 @@ class submitJobs(object):
                 raise IOError('Could not submit slurm job')
             # store succesfully commoted commands
             slurmJobList.append((slurmID, command, processors, memory, stdout,
-                stderr, dependOut))
+                stderr, moduleOut, dependOut))
             # print commands if requested
             if verbose:
                 print(
@@ -110,10 +118,28 @@ class submitJobs(object):
                     'memory:       {}\n'\
                     'stdout:       {}\n'\
                     'stderr:       {}\n'\
+                    'modules:      {}\n'\
                     'dependencies: {}\n'.format(
                         slurmID, command, processors, memory, stdout, stderr,
-                        str(dependOut)
+                        str(moduleOut), str(dependOut)
                      )
                 )
         # save command to output file
         return(slurmJobList)
+
+def parsePathModule(pmFile):
+    # Create output dictionary and open file
+    pmDict = {}
+    with open(pmFile) as inFile:
+        # Extract data from file
+        for line in inFile:
+            lineData = line.strip().split('\t')
+            program = lineData[0]
+            # Store paths and modules for each program
+            pmDict[(program, 'path')] = lineData[1]
+            if len(lineData) == 2:
+                pmDict[(program, 'modules')] = []
+            else:
+                pmDict[(program, 'modules')] = lineData[2:]
+    # Return data
+    return(pmDict)
